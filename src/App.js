7eca02d1cae52e9c8912067e4a46e6c451e88088
souravsharma-dev <sourav.sourav.sharma07@gmail.com>
2026-01-01 22:44:05 +0530

    Initial commit - React project setup

---
+++ src/App.js
@@ -0,0 +1,19 @@
+function App() {
+  return (
+  <div>
+    <h1 style={{
+      fontSize: '32px',
+      color: 'red'
+    }}>Hello My Name is sourav</h1>
+    <p>This is a simple app to demonstrate a React component.</p>
+    <table>
+    <th>Table
+    </th>
+     <tr>Sourav</tr>
+     <tr>Surav</tr>
+     </table>
+  </div>
+)
+}
+
+export default App;
