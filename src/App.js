9c9ff504e6f3d28431b8a6552e3af65a37a00dbd
souravsharma-dev <sourav.sourav.sharma07@gmail.com>
2026-01-05 11:13:35 +0530

    Changes in UI

--- src/App.js
+++ src/App.js
@@ -24,5 +24,6 @@ function App() {
   </div>
 )
 }
+import Footer from './Footer.jsx';
  
 export default App;
---
+++ src/Footer.jsx
@@ -0,0 +1,5 @@
+
+function Footer(){
+<div><h1>Footer</h1></div>
+}
+export default Footer;
